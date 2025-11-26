8253502ae5bf8af76ce4c4b058ac793192b91398
IagoFP2004 <iagofranciscoperez123@gmail.com>
2025-11-26 15:30:23 +0100

    Arreglado problema responsive

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,16 +1,20 @@
 export function Header() {
   return (
     <header className="w-100 bg-dark text-white">
-      <nav className="navbar navbar-expand-lg navbar-dark">
-        <div className="container-fluid d-flex justify-content-between align-items-center me-5 ms-5"> 
-          {/* Logo */}
-          <img
-            style={{ width: '200px' }}
-            src="./IagoFP2004_white.png"
-            alt="Logo IagoFP2004"
-          />
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
 
-          {/* Botón hamburguesa */}
+          {/* Logo / Brand */}
+          <a className="navbar-brand d-flex align-items-center" href="#">
+            <img
+              style={{ width: "160px" }} // si ves que no cabe, baja un poco este número
+              src="./IagoFP2004_white.png"
+              alt="Logo IagoFP2004"
+              className="d-inline-block align-text-top"
+            />
+          </a>
+
+          {/* Botón hamburguesa (sale solo en responsive) */}
           <button
             className="navbar-toggler"
             type="button"
@@ -23,36 +27,37 @@ export function Header() {
             <span className="navbar-toggler-icon" />
           </button>
 
-          {/* Menú colapsable */}
+          {/* Menú: colapsado en móvil, en línea en >= lg */}
           <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ms-auto">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link text-white" href="#">
-                  <i class="bi bi-house p-2"></i>Inicio
+                  <i className="bi bi-house p-2" />Inicio
                 </a>
               </li>
               <li className="nav-item">
                 <a className="nav-link text-white" href="#">
-                  <i class="bi bi-person p-2"></i>Sobre Mi
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link text-white " href="#">
-                  <i class="bi bi-list-ol p-2"></i>Habilidades
+                  <i className="bi bi-person p-2" />Sobre mí
                 </a>
               </li>
               <li className="nav-item">
                 <a className="nav-link text-white" href="#">
-                  <i class="bi bi-folder-fill p-2"></i>Proyectos
+                  <i className="bi bi-list-ol p-2" />Habilidades
                 </a>
               </li>
-               <li className="nav-item">
+              <li className="nav-item">
                 <a className="nav-link text-white" href="#">
-                  <i class="bi bi-envelope p-2"></i>Contacto
+                  <i className="bi bi-folder-fill p-2" />Proyectos
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-white" href="#">
+                  <i className="bi bi-envelope p-2" />Contacto
                 </a>
               </li>
             </ul>
           </div>
+
         </div>
       </nav>
     </header>
