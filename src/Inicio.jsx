89046bc98be35ea3f796b8346e4bd07962b723e3
IagoFP2004 <iagofranciscoperez123@gmail.com>
2025-11-26 16:07:53 +0100

    Agregada imagen de inicio

--- src/Inicio.jsx
+++ src/Inicio.jsx
@@ -3,10 +3,12 @@ export function Inicio() {
     <div 
     style={{
         backgroundImage: 'url(/ImagenInicio.png)', 
-        height: '92vh', backgroundSize: 'cover',
+        height: '95vh', backgroundSize: 'cover',
         display: 'flex', flexDirection: 'column',
         justifyContent: 'center', alignItems: 'center',
-        color: 'white', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.7)'
+        color: 'white', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.7)',
+
+
     }}
     >
       <h1>Bienvenido a mi Portafolio</h1>
