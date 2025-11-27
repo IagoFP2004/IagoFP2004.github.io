d8c8f53d90c8d757384a86e6dce0c6edbd982021
IagoFP2004 <iagofranciscoperez123@gmail.com>
2025-11-27 12:09:46 +0100

    Agregado  video presentacion NutroPRo

--- src/Habilidades.jsx
+++ src/Habilidades.jsx
@@ -3,7 +3,8 @@ export function Habilidades() {
     <div className="row row-cols-1 row-cols-md-3 justify-content-center g-4 mb-5 px-3 px-md-5">
       
       <div className="col">
-        <div className="p-4 h-100 d-flex flex-column align-items-center rounded-3 text-center shadow-lg bg-body-tertiary border border border-primary">
+        <div className="p-4 h-100 d-flex flex-column align-items-center rounded-3 text-center shadow-sm bg-body-tertiary border border border
+        ">
           <h3>Frontend</h3>
           <img
             src="https://skillicons.dev/icons?i=js,html,css,bootstrap,tailwind,react"
@@ -14,7 +15,8 @@ export function Habilidades() {
       </div>
 
       <div className="col">
-        <div className="p-4 h-100 d-flex flex-column align-items-center rounded-3 text-center shadow-lg bg-body-tertiary border border border-primary">
+        <div className="p-4 h-100 d-flex flex-column align-items-center rounded-3 text-center shadow-sm bg-body-tertiary border border border
+        ">
           <h3>Backend</h3>
           <img
             src="https://skillicons.dev/icons?i=php,symfony,mysql,docker,nginx,java"
@@ -25,7 +27,8 @@ export function Habilidades() {
       </div>
 
       <div className="col">
-        <div className="p-4 h-100 d-flex flex-column align-items-center rounded-3 text-center shadow-lg bg-body-tertiary border border border-primary">
+        <div className="p-4 h-100 d-flex flex-column align-items-center rounded-3 text-center shadow-sm bg-body-tertiary border border border
+        ">
           <h3>Tools</h3>
           <img
             src="https://skillicons.dev/icons?i=git,github,linux,vscode,postman,phpstorm,figma"
--- src/Proyectos.jsx
+++ src/Proyectos.jsx
@@ -1,10 +1,35 @@
+import { useState } from "react";
 export function Proyectos() {
+  const [hoveredCard, setHoveredCard] = useState(false);
   return (
-    <div className="d-flex justify-content-center">
-      <div className="d-flex p-3 justify-content-center align-items-center bg-warning rounded-3 mb-5 w-75 opacity-75">
-        <p className="m-0 text-center">
-          <i class="bi bi-exclamation-triangle-fill pe-2"></i>Los proyectos serán agregados próximamente
-        </p>
+    <div className="d-flex justify-content-center gap-4 flex-wrap mb-5">
+      <div>
+        <div className="card" style={{ width: "18rem", width: hoveredCard ? "450px" : "286px",
+              transition: "width 0.3s ease" }}>
+          <img
+            onMouseEnter={() => setHoveredCard(true)}
+            onMouseLeave={() => setHoveredCard(false)}
+            src={hoveredCard ? "/NutroPro.gif" : "/NutroPro.png"}
+            style={{
+              width: hoveredCard ? "450px" : "286px",
+              transition: "width 0.3s ease"
+            }}
+            className="card-img-top"
+            alt="imagen proyecto"
+          />
+          <div className="card-body">
+            <h5 className="card-title">NutroPro</h5>
+            <p className="card-text">
+              Proyecto de fin de ciclo de DAW. Este proyecto simula una tienda
+              online de suplementación deportiva, donde los usuarios pueden
+              registrarse, iniciar sesión, ver productos, añadirlos al carrito
+              y realizar pedidos.
+            </p>
+            <a href="https://github.com/IagoFP2004/NutroPro" target="_blank" className="btn btn-dark d-flex justify-content-center">
+              <i className="bi bi-github pe-2"></i>Ver en GitHub
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
